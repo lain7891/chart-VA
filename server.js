bcd31d97c5201cfe34c91f46177e583292aaeeb8
Estefania <nialupo87@gmail.com>
2021-05-12 22:08:39 -0400

    initial setup complete

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/chart-VA", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
+app.use(express.static("client/build"));
+
+app.get("/api/config", (req, res) => {
+    res.json({
+      success: true,
+    });
+  });
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
