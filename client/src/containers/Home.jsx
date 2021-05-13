82e2baae2c30973f5bc441920ad50a4db036885b
Estefania <nialupo87@gmail.com>
2021-05-12 22:43:33 -0400

    added a home page

---
+++ client/src/containers/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+        <div className="row">
+        <div className="col">
+            <h1>Behavior Chart</h1>
+        </div>
+        </div>
+       
+      </div>
+    );
+};
+
+export default Home;
