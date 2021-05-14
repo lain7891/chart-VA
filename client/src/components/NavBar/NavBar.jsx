bd811ae1f93a4c338abc4c04600dd0e2324e5d54
Estefania <nialupo87@gmail.com>
2021-05-13 20:11:04 -0400

    getting inputs in the name form

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import {NavLink, Link} from "react-router-dom";
 import "./NavBar.css"
 
+const style = {
+  navBar: {
+fontSize: 30,
+
+  },
+  text: {
+    color: "#ffffff"
+  }
+}
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-dark bg-primary" style={style.navBar}>
         <div className="container-fluid">
-         <Link to="/" className="brand-logo">
+         <Link to="/" className="brand-logo" style={style.text}>
            Behavior Chart
          </Link>
-          <ul className="active">
-       <NavLink to="/student">Student</NavLink>
+          <ul className="active" >
+       <NavLink to="/student" style={style.text}>Student</NavLink>
         </ul>
         </div>
       </nav>
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,16 +1,76 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+
+const style = {
+    button: {
+        marginTop: "3rem"
+    }
+}
 
 const Home = () => {
-    return (
-        <div className="container">
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+
+  const handleFormSubmit = (e) => {
+      e.preventDefault();
+      axios.post("/api/children")
+        .then((response) => {
+            console.log(response.data);
+            history.push("/admin");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        
+  }
+  return (
+    <>
+      <div className="container">
         <div className="row">
-        <div className="col">
+          <div className="col">
             <h1>Behavior Chart</h1>
+          </div>
+          <form class="row g-3 needs-validation" onSubmit={handleFormSubmit}>
+            <div className="col-md-4">
+              <input
+                placeholder="First Name"
+                type="text"
+                id="firstName"
+                className="form-control"
+                name="firstName"
+                value={firstName}
+                onChange={(e)=>{
+                    setFirstName(e.target.value)
+                }}
+              />
+              <div className="valid-feedback">Looks good!</div>
+            </div>
+            <div className="col-md-4">
+              <input
+                placeholder="Last Name"
+                type="text"
+                id="lastName"
+                className="form-control"
+                name="lastName"
+                value={lastName}
+                onChange={(e)=> {
+                    setLastName(e.target.value)
+                }}
+              />
+              <div className="valid-feedback">Looks good!</div>
+            </div>
+          </form>
         </div>
-        </div>
-       
       </div>
-    );
+      <div className="container" style={style.button}>
+        <div className="row">
+          <div className="col">
+            <input class="btn btn-primary" type="submit" value="Submit"></input>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
